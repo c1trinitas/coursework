fe882dcb4cc7921d2f3657d005d0cd68f80a45b8
citrinitas <daniargizatullin8@gmail.com>
2024-04-04 02:38:35 +0500

    cw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        App app = new App();
+        app.printEmployees();
+        System.out.println("Самой низкой зарплатой среди сотрудников обладает: " + app.minSalary());
+        System.out.println("Самой высокой зарплатой среди сотрудников обладает: " + app.maxSalary());
+        System.out.println(app.sumOfSalaries() + " Сумма зарплат");
+        System.out.println(app.averageSalary() + " Средняя зарплата");
     }
 }
